82c09062f8eff0bf64f07ccba1bb0e6fcfc6d331
jason <jzq@163.com>
2023-04-05 00:33:40 +0800

    自动生成的springboot 基本文件

---
+++ src/main/java/com/nvtek/mpine/MpineApplication.java
@@ -0,0 +1,13 @@
+package com.nvtek.mpine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nvtek/mpine/MpineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvtek.mpine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
